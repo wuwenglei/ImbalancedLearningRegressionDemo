0c5dd98e93165410ea780d30339cd2575fee8f4d
wuwenglei <wuwenglei2020@gmail.com>
2024-09-20 16:15:27 +0800

    Initial commit

---
+++ test/cdk-imbalanced-learning-regression-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkImbalancedLearningRegressionDemo from '../lib/cdk-imbalanced-learning-regression-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-imbalanced-learning-regression-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkImbalancedLearningRegressionDemo.CdkImbalancedLearningRegressionDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
